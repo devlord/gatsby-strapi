31a53d535ebf46fdcaddda999af7731ac8846f30
devlord <dev@qupital.com>
2019-09-09 16:11:48 +0800

    added markdown support

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
+import ReactMarkdown from "react-markdown"  
+import "../styles/global.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -15,7 +17,13 @@ const IndexPage = ({ data }) => (
             <Link to={`/${document.node.id}`}>{document.node.title}</Link>
           </h2>
           <Img fixed={document.node.image.childImageSharp.fixed}/>
-          <p>{document.node.content}</p>
+          <ReactMarkdown  
+            source={document.node.content.substring(0, 500).concat("...")}
+            transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
+            className="indexArticle"
+            escapeHtml={false}
+          />
+          <Link to={`/${document.node.id}`}>Read more</Link>
         </li>
       ))}
     </ul>
